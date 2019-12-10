bd2315f101072ebbea20f053f78fc30589ca4d89
Sören Oldag <soeren_oldag@freenet.de>
2019-12-10 16:34:28 +0100

    Read requirements of package from requirements.txt

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 with open('README.rst') as rd:
     long_description = rd.read()
 
+with open("requirements.txt") as rq:
+    requirements = rq.read().splitlines()
+
 setup(
     name='spotify_token',
     py_modules=['spotify_token'],
@@ -12,7 +15,7 @@ setup(
     author_email='egonzalezh94@gmail.com',
     license='MIT',
     url='https://github.com/enriquegh/spotify-webplayer-token',
-    install_requires=['requests'],
+    install_requires=requirements,
     long_description=long_description,
     keywords=['wrapper', 'spotify'],
     classifiers=["Programming Language :: Python :: 3",
